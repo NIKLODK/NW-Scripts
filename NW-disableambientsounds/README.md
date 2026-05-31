f4725f44eec314223f109cf43d316eac182cbca6
Niklas <148580250+NIKLODK@users.noreply.github.com>
2026-06-01 01:11:57 +0200

    Delete NW-disableambientsounds directory

--- NW-disableambientsounds/README.md
+++
@@ -1,4 +0,0 @@
-# NS-disableambientsounds
-This resource provides the removal of annoying ambient sounds, such as gun sounds at the PDM, and more.
-
-https://discord.gg/kqTcSkKr
--- NW-disableambientsounds/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-games { 'gta5' }
-author 'NIKLO'
-description 'Disable Ambient Sounds, removes all gta sound effects.'
-version '4.0.4'
-client_script 'main.lua'
